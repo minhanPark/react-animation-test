4d5e78780a2cecf9bf761bec91cb5b123c12cb3d
Running water <alsgks1218@naver.com>
2022-03-06 17:47:11 +0900

    variants test

--- src/App.tsx
+++ src/App.tsx
@@ -13,19 +13,56 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: white;
-  border-radius: 15px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
+
+const Circle = styled(motion.div)`
+  background-color: white;
+  height: 70px;
+  width: 70px;
+  place-self: center;
+  border-radius: 35px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const boxVar = {
+  first: { opacity: 0, scale: 0.5 },
+  second: {
+    scale: 1,
+    opacity: 1,
+    transition: {
+      type: "spring",
+      duration: 0.5,
+      bounce: 0.5,
+      delayChildren: 0.5,
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const circleVar = {
+  first: {
+    opacity: 0,
+    y: 10,
+  },
+  second: {
+    opacity: 1,
+    y: 0,
+  },
+};
 
 function App() {
   return (
     <Wrapper>
-      <Box
-        transition={{ type: "spring", delay: 0.5 }}
-        initial={{ scale: 0 }}
-        animate={{ scale: 1, rotateZ: 360 }}
-      />
+      <Box variants={boxVar} initial="first" animate="second">
+        {[0, 0, 0, 0].map((circle) => (
+          <Circle variants={circleVar} />
+        ))}
+      </Box>
     </Wrapper>
   );
 }
